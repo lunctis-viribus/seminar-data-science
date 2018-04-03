745debc81b5f5e56582d76613d884aeafb9b4fc2
lunctis-viribus <a@a.a>
2018-04-03 14:18:09 +0200

    Working image sharpening

--- myTask.m
+++ myTask.m
@@ -1,14 +1,7 @@
 image=imread('blurryImage.png');
 [h w d]=size(image);
 U = double(reshape(image,w*h,d))/255;
-%% Write your method here
-kernel = -1 * ones(3)/9
-kernel(2,2) = 8;
-kernel = kernel / sum(kernel(:)); % Normalize sum to 1.
-
-
-%% I matrix
-%%
+%% Construct gradient matrix
 r = [];
 c = [];
 v = [];
@@ -17,12 +10,10 @@ for k = 1:h
     j = 1;
     for i = 1:w-1
         r = [r,(k-1)*(w-1)+i];
-%         c = [c,j+(k-1)*(h-1)];
         c = [c,k+(h*(i-1))];
         v = [ v,-1];
         
         r = [r,(k-1)*(w-1)+i];
-%         c = [c,j+h+(k-1)*(h-1)];
         c = [c,k+(h*(i))];
         v = [v,1];
         j = j + h;
@@ -44,20 +35,20 @@ for k = 1:w
         
     end
 end
-
-M1 = sparse(r,c,v,(h-1)*w+(w-1)*h,h*w);
-%%
-    
-        
-
-
-% High frequency boost filter
-sharpenedImage = conv2(double(U), kernel, 'same');
-
-
-sharpenedImage =uint8(reshape(sharpenedImage,h,w,d)*255);
-
+G = sparse(r,c,v,(h-1)*w+(w-1)*h,h*w);
+%% Solve linear system
+size=size(G);
+N=size(2);
+g=G*U;
+cs = 3;
+cu = .5;
+p=sparse(N,N,pi);
+Id=eye(N,'like',p);
+A = G'*G+cu*Id;
+B = cs*G'*g+cu*U;
+Uinv = A\B;
+%% Show images
+sharpenedImage =uint8(reshape(Uinv,h,w,d)*255); 
 figure
-subplot(1,2,1), imshow(image)
-subplot(1,2,2), imshow(sharpenedImage)
+imshowpair(image,sharpenedImage,'montage')
 imwrite(sharpenedImage,'out.png')
