5f2de7a9caf6fb2d7018cde06fb1b3b709441096
KinLok <kinlok@msn.com>
2018-04-08 22:08:42 +0200

    Demo

---
+++ demo_ver4.m
@@ -0,0 +1,46 @@
+% Supress warnings
+warning('off','all')
+
+target = imread('./img/air.jpg');
+source = imread('./img/squirrel.jpg');
+mask = imread('./img/squirrel_mask.jpg');
+
+%default region of the target image for stitching the source image
+offsetX = 150;
+offsetY = 50;
+
+%select the region for cropping and stitching manually (manually_select = 1) or not(manually_select = 0)
+manually_select = 0;
+
+%use the mask or not (if using the mask, set it to 1)
+mask_selected = 1;
+
+% Insert image automatically on given coordinates
+output = imageBlending(source,target,manually_select, mask, offsetX,offsetY);
+f2 = figure(2);
+imshow(output);
+disp('Press a key to continue');
+pause;
+close(f2);
+
+% Manually give position image
+
+offsetX = 300;
+offsetY = 450;
+
+source = imread('./img/fg.jpg');
+mask = imread('./img/mask2.jpg');
+output = imageBlending(source,output,manually_select, mask, offsetX,offsetY);
+
+f2 = figure(2);
+imshow(output);
+disp('Press a key to continue');
+pause;
+close(f2);
+
+manually_select = 1;
+output = imageBlending(source,output,manually_select, mask);
+imshow(output);
+disp('Press a key to continue');
+pause;
+close(f2);
