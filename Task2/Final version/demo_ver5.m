71c6bc9d89e117ef747fed328db4033ccef4aec8
Jipjop <kinlok@msn.com>
2018-04-08 23:33:28 +0200

    Clean code

--- Task2/Final version/demo_ver5.m
+++ Task2/Final version/demo_ver5.m
@@ -16,17 +16,9 @@ warning('off','all')
 %%% Using images of the airplane into the mountain
 target = imread('./img/bg.jpg');
 source = imread('./img/fg.jpg');
-mask = imread('./img/mask2.jpg');
-
-%default region of the target image for stitching the source image
-offsetX = 150;
-offsetY = 50;
 
 %select the region for cropping and stitching manually (manually_select = 1) or not(manually_select = 0)
 manually_select = 1;
-
-%use the mask or not (if using the mask, set it to 1)
-mask_selected = 0;
 
 output = imageBlending(source,target,manually_select);
 imshow(output);
@@ -52,9 +44,6 @@ offsetY = 150;
 
 %select the region for cropping and stitching manually (manually_select = 1) or not(manually_select = 0)
 manually_select = 0;
-
-%use the mask or not (if using the mask, set it to 1)
-mask_selected = 1;
 
 % Insert image automatically on given coordinates
 output = imageBlending(source,target,manually_select, mask, offsetX,offsetY);
